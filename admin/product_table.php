aa229197fab7e8822c2d248561cd6f6438f6cfc0
vbg2002 <vgayotayan@gmail.com>
2023-07-07 22:07:40 +0800

    Update product_table.php

--- admin/product_table.php
+++ admin/product_table.php
@@ -329,7 +329,7 @@ if ($select_products->rowCount() > 0) {
          <div class="inputBox">
          <input type="hidden" name="ProductlineName">
          <span>product line (required)</span>
-         <select name="ProductlineName" id="sort-item" class="select" required>
+         <select name="ProductlineName" id="sort-item" class="select box" required>
                <?php
                   $select_productline = $conn->prepare("SELECT * FROM `productline`"); 
                   $select_productline->execute();
@@ -422,6 +422,35 @@ $(document).ready(function() {
 });
 
 </script>
+
+<script>
+   // Get the modal
+   var modal = document.getElementById("myModals");
+
+   // Get the button that opens the modal
+   var btn = document.getElementById("myBtnsss");
+
+   // Get the <span> element that closes the modal
+   var span = document.getElementsByClassName("close")[0];
+
+   // When the user clicks the button, open the modal 
+   btn.onclick = function() {
+   modal.style.display = "block";
+   }
+
+   // When the user clicks on <span> (x), close the modal
+   span.onclick = function() {
+   modal.style.display = "none";
+   }
+
+   // When the user clicks anywhere outside of the modal, close it
+   window.onclick = function(event) {
+   if (event.target == modal) {
+      modal.style.display = "none";
+   }
+   }
+</script>
+
 
 
 
